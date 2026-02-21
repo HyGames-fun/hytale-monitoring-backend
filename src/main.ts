1fae3dd55a61488dbeb9cff0fb8270bd1132d138
Snuslyk <127945008+Snuslyk@users.noreply.github.com>
2026-02-22 02:49:09 +0300

    feat: scalar api documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import cookieParser from 'cookie-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -17,6 +19,21 @@ async function bootstrap() {
   )
 
   app.use(cookieParser())
+
+  const config = new DocumentBuilder()
+    .setTitle('Hytale Monitoring')
+    .setDescription('Hytale monitoring API documentation')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+    }),
+  )
 
   await app.listen(process.env.PORT ?? 3000)
 }
